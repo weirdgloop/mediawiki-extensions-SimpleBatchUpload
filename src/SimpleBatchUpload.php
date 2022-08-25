96e24af9faafa265e432df6678dc894c83cf3d3e
TehKittyCat <tehkittycat@weirdgloop.org>
2022-08-24 20:35:51 -0400

    Fix compatibility with PHP 8.1 and utilise extension.json where possible.

--- src/SimpleBatchUpload.php
+++ src/SimpleBatchUpload.php
@@ -22,6 +22,8 @@
 
 namespace SimpleBatchUpload;
 
+use MediaWiki\Hook\MakeGlobalVariablesScriptHook;
+use MediaWiki\Hook\ParserFirstCallInitHook;
 use Parser;
 
 /**
@@ -29,125 +31,17 @@ use Parser;
  *
  * @package SimpleBatchUpload
  */
-class SimpleBatchUpload {
-
-	/**
-	 * @var array Max files could be uploaded per batch
-	 */
-	protected $maxFilesPerBatchConfig;
-
-	public static function initCallback() {
-
-		$simpleBatchUpload = new self();
-		$simpleBatchUpload->registerEarlyConfiguration( $GLOBALS );
-	}
-
-	public function registerEarlyConfiguration( &$targetConfiguration ){
-		$sourceConfiguration = $this->getEarlyConfiguration();
-		$this->mergeConfiguration( $sourceConfiguration, $targetConfiguration );
-	}
-
-	public function registerLateConfiguration( &$targetConfiguration ){
-		$sourceConfiguration = $this->getLateConfiguration();
-		$this->mergeConfiguration( $sourceConfiguration, $targetConfiguration );
-	}
-
-	/**
-	 * @param array $targetConfiguration
-	 */
-	protected function mergeConfiguration( $sourceConfiguration, &$targetConfiguration ) {
-		foreach ( $sourceConfiguration as $varname => $value ) {
-			$targetConfiguration[ $varname ] = array_key_exists( $varname, $targetConfiguration )?array_replace_recursive( $targetConfiguration[ $varname ], $value ):$value;
-		}
-	}
-
-	protected function getEarlyConfiguration(): array {
-
-		$configuration = [];
-
-		$configuration[ 'wgExtensionMessagesFiles' ][ 'SimpleBatchUploadAlias' ] = __DIR__ . '/SimpleBatchUpload.alias.php';
-		$configuration[ 'wgExtensionMessagesFiles' ][ 'SimpleBatchUploadMagic' ] = __DIR__ . '/SimpleBatchUpload.magic.php';
-
-		$configuration[ 'wgSpecialPages' ][ 'BatchUpload' ] = '\SimpleBatchUpload\SpecialBatchUpload';
-
-		$configuration[ 'wgHooks' ][ 'ParserFirstCallInit' ][ 'ext.simplebatchupload' ] = [ $this, 'registerParserFunction' ];
-		$configuration[ 'wgHooks' ][ 'MakeGlobalVariablesScript' ][ 'ext.simplebatchupload' ] = [ $this, 'onMakeGlobalVariablesScript' ];
-		$configuration[ 'wgHooks' ][ 'SetupAfterCache' ][ 'ext.simplebatchupload' ] = [ $this, 'onSetupAfterCache'];
-
-		return $configuration;
-	}
-
-	protected function getLateConfiguration(): array {
-
-		$configuration = [];
-		$configuration[ 'wgResourceModules' ] = $this->getUploadSupportModuleDefinition() + $this->getUploadModuleDefinition();
-
-		return $configuration;
-	}
+class SimpleBatchUpload implements
+	MakeGlobalVariablesScriptHook,
+	ParserFirstCallInitHook
+{
 
 	/**
 	 * @param \Parser $parser
-	 *
-	 * @return bool
 	 * @throws \MWException
 	 */
-	public function registerParserFunction( &$parser ) {
+	public function onParserFirstCallInit( $parser ) {
 		$parser->setFunctionHook( 'batchupload', [ new UploadButtonRenderer(), 'renderParserFunction' ], Parser::SFH_OBJECT_ARGS );
-		return true;
-	}
-
-	protected function getUploadSupportModuleDefinition(): array {
-		if ( version_compare( $GLOBALS[ 'wgVersion' ], '1.35.0', '>=' ) ) {
-			$dependencies = [ 'jquery.ui' ];
-		} else {
-			$dependencies = [ 'jquery.ui.widget' ];
-		}
-
-		return [ 'ext.SimpleBatchUpload.jquery-file-upload' =>
-
-			$this->getBasePathsForNonComposerModules() +
-
-			[
-				'scripts' => [ 'res/jquery.fileupload.js' ],
-				'styles' => [ 'res/jquery.fileupload.css' ],
-				'position' => 'top',
-				'dependencies' => $dependencies,
-			],
-		];
-	}
-
-	protected function getUploadModuleDefinition(): array {
-
-		$dependencies = [ 'ext.SimpleBatchUpload.jquery-file-upload', 'mediawiki.Title', 'mediawiki.jqueryMsg' ];
-
-		if ( version_compare( $GLOBALS[ 'wgVersion' ], '1.32.0', '>' ) ) {
-			$dependencies[] = 'mediawiki.api';
-		} else {
-			$dependencies[] = 'mediawiki.api.edit';
-		}
-
-		return [ 'ext.SimpleBatchUpload' =>
-
-			$this->getBasePathsForNonComposerModules() +
-
-			[
-				'scripts' => [ 'res/ext.SimpleBatchUpload.js' ],
-				'styles' => [ 'res/ext.SimpleBatchUpload.css' ],
-				'position' => 'top',
-				'dependencies' => $dependencies,
-				'messages' => [ 'simplebatchupload-comment', 'simplebatchupload-max-files-alert' ],
-			],
-		];
-	}
-
-	/**
-	 * @return string[]
-	 */
-	protected function getBasePathsForNonComposerModules(): array {
-		return [
-			'localBasePath' => dirname( __DIR__ ),
-			'remoteBasePath' => $GLOBALS[ 'wgExtensionAssetsPath' ] . '/SimpleBatchUpload',
-		];
 	}
 
 	/**
@@ -155,22 +49,7 @@ class SimpleBatchUpload {
 	 * @param \OutputPage $out
 	 */
 	public function onMakeGlobalVariablesScript( &$vars, $out ) {
-		$vars['simpleBatchUploadMaxFilesPerBatch'] = $this->getMaxFilesPerBatchConfig();
-	}
-
-	public function onSetupAfterCache() {
-		$this->registerLateConfiguration( $GLOBALS );
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getMaxFilesPerBatchConfig() {
-
-		if ( $this->maxFilesPerBatchConfig === null ) {
-			$this->maxFilesPerBatchConfig = $GLOBALS[ 'wgSimpleBatchUploadMaxFilesPerBatch' ];
-		}
-
-		return $this->maxFilesPerBatchConfig;
+		global $wgSimpleBatchUploadMaxFilesPerBatch;
+		$vars['simpleBatchUploadMaxFilesPerBatch'] = $wgSimpleBatchUploadMaxFilesPerBatch;
 	}
 }
